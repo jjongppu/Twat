776e20aa6e5984f81ec3b18153dcb84c54d6d680
김종길 <jjongppu@naver.com>
2017-06-15 20:05:11 +0900

    어드민 홈에 카운트해온 정보들 뿌려줄 다오 추가

--- admin/dao/AdminDAO.java
+++ admin/dao/AdminDAO.java
@@ -78,83 +78,37 @@ public class AdminDAO {
 		}
 	   
 		
-	   // 회원 가입을 위한 메서드 ------승우----------------------------
-	   public int signUpMember(String MEMBER_ID, String MEMBER_PW, String MEMBER_NAME, String MEMBER_PHONE, String MEMBER_IMG, String MEMBER_GENDER, String MEMBER_BIRTH ) {
-	      
-	      int result = -1;
-	      
-	      String insertSql = "insert into MEMBER values(?,?,?,?,?,?,?,?,?,?)";
-	      
-	      try {
-	         con = getConnection();
-	         psmt = con.prepareStatement(insertSql);
-	      
-	         psmt.setString(1, MEMBER_ID);
-	         psmt.setString(2, MEMBER_PW);
-	         psmt.setString(3, MEMBER_NAME);
-	         psmt.setString(4, MEMBER_PHONE);
-	         psmt.setString(5, MEMBER_IMG);
-	         psmt.setString(6, MEMBER_GENDER);
-	         psmt.setString(7, MEMBER_BIRTH);
-	         psmt.setString(8, null);
-	         psmt.setString(9, null);
-	         psmt.setTimestamp(10, null);
-	         
-	         result = psmt.executeUpdate();
-	      
-
-	      } catch (Exception e) {
-	         e.printStackTrace();
-	      } finally {
-	         try {
-	            if(rs != null)   rs.close();
-	            if(psmt != null) psmt.close();
-	            if(con != null) con.close();
-	         } catch (SQLException e) {
-	            e.printStackTrace();
-	         }
-	   }
-	      return result;      
-	   }
+		// 쫑길빵길의 어드민 홈 db정보 뿌려주기
+		public ArrayList<Integer> getAllInfo(){
+			ArrayList<Integer> info = new ArrayList<Integer>();
+			String selectGetInfo = "SELECT"; 
+			selectGetInfo +="(SELECT COUNT(*) FROM member) as memC,";
+			selectGetInfo +="(SELECT COUNT(*) FROM qna) as qnaC,";
+			selectGetInfo +="(SELECT COUNT(*) FROM calendar where CAL_DEPTH=0) as calC,";
+			selectGetInfo +="(SELECT VISIT_COUNT FROM visit where VISIT_KIND='TOTAL') as visC";						
+			try {
+				con = getConnection();
+				psmt = con.prepareStatement(selectGetInfo);
+				rs = psmt.executeQuery();
+				if(rs.next()) {
+					info.add(rs.getInt("memC"));
+					info.add(rs.getInt("qnaC"));
+					info.add(rs.getInt("calC"));
+					info.add(rs.getInt("visC"));
+				}
+			} catch (Exception e) {
+				e.printStackTrace();
+			} finally {
+					try {
+						if(rs != null)
+						if(psmt != null) psmt.close();
+						if(con != null) con.close();
+					} catch (SQLException e) {
+						e.printStackTrace();
+					}
+			}
+			return info;
+		}
 	   
-	   
-	 //회원정보를 가져오고싶어함------------------------------------------------
-	   public ArrayList<MemberVO> myInfo(String MEMBER_ID){
-	      String sql = "select MEMBER_IMG, MEMBER_NAME, MEMBER_PHONE, MEMBER_BIRTH from MEMBER where MEMBER_ID = ? ";
-	      
-	      ArrayList<MemberVO> arList = new ArrayList<MemberVO>(); 
-	      
-	      try {
-	         con = getConnection();
-	          psmt = con.prepareStatement(sql);
-	            psmt.setString(1, MEMBER_ID);
-	            rs = psmt.executeQuery();
-	            
-	            //이렇게하면은 rs.getstring(1)에 이미지 2에 이름 3에 전화번호 4에 생일이 들어온다.
-	            while(rs.next()){
-	               MemberVO mvo = new MemberVO();
-	               mvo.setMEMBER_IMG(rs.getString("MEMBER_IMG"));
-	               mvo.setMEMBER_NAME(rs.getString("MEMBER_NAME"));
-	               mvo.setMEMBER_PHONE(rs.getString("MEMBER_PHONE"));
-	               mvo.setMEMBER_BIRTH(rs.getString("MEMBER_BIRTH"));
-	                 arList.add(mvo);
-	            }
-	            //arList에 이미지 이름 폰번호 생일이 나온다.
-	         
-	      } catch (Exception e) {
-	      }finally {
-	            try {
-	                  if(rs != null)
-	                  if(psmt != null) psmt.close();
-	                  if(con != null) con.close();
-	               } catch (SQLException e) {
-	               }
-	         }
-	      return arList;
-	   }
-	   
-	   
-
-	
 	
 }
