a92140bf1f141ae0568874941f3866e0389fcfd1
SungHoonKim <martino1114@naver.com>
2017-06-21 23:15:32 +0900

    이것저것 수정

--- src/com/twat/dao/CalendarDAO.java
+++ src/com/twat/dao/CalendarDAO.java
@@ -64,6 +64,7 @@ public class CalendarDAO
 			{
 				CalendarVO schedule = new CalendarVO();
 				schedule.setCal_num(rSet.getInt(1));
+				schedule.setCal_time(rSet.getTimestamp(2));
 				schedule.setCal_date(rSet.getString(3));
 				schedule.setGroup_id(rSet.getInt(4));
 				schedule.setCal_memo(rSet.getString(5));
@@ -121,6 +122,7 @@ public class CalendarDAO
 				CalendarVO schedule = new CalendarVO();
 				
 				schedule.setCal_num(rSet.getInt(1));
+				schedule.setCal_time(rSet.getTimestamp(2));
 				schedule.setCal_date(rSet.getString(3));
 				schedule.setGroup_id(rSet.getInt(4));
 				schedule.setCal_memo(rSet.getString(5));
