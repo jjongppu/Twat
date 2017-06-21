704c94f7e021be633f263f9b3476d42961ced168
빠빠이용수 <csw1266@gmail.com>
2017-06-21 18:09:23 +0900

    qnaDao 글쓰기 하는중 // 건의사항 글쓰기html 수정 // qna쓰기 서블릿 수정중

--- src/com/twat/dao/QnaDAO.java
+++ src/com/twat/dao/QnaDAO.java
@@ -3,6 +3,7 @@ package com.twat.dao;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
+import java.sql.SQLException;
 
 import javax.naming.Context;
 import javax.naming.InitialContext;
@@ -32,9 +33,78 @@ public class QnaDAO {
 	}
 	
 	
-	// 메서드들 들어감
-//	public int insertQnA(String ) {
-//		
-//	}
+	// 건의사항 글쓰기 --------------------------최승우-------------------
+	public int insertQnA(String qnaId, String qnaCategory, int qnaPW, String qnaTitle, String qnaContents) {
+		int result = -1;
+//		int qna_number = 0;
 		
+		
+		String insertSql = "insert into qna values(?, ?, ?, ?, ?, ?, CURRENT_TIMESTAMP, ?)";
+		
+		try {
+			con = getConnection();
+			psmt = con.prepareStatement(insertSql);
+			
+			psmt.setInt(1, getMaxNum());
+			psmt.setString(2, qnaId);
+			psmt.setString(3, qnaCategory);
+			psmt.setInt(4, qnaPW);
+			psmt.setString(5, qnaTitle);
+			psmt.setString(6, qnaContents);
+			psmt.setString(7, null);
+			
+			result = psmt.executeUpdate();			
+			
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} finally {
+	        try {
+	        	if(rs != null) rs.close();
+		        if(psmt != null) psmt.close();
+		        if(con != null) con.close();
+	        } catch (SQLException e) {
+	        	// TODO Auto-generated catch block
+		        e.printStackTrace();
+	        }
+//	        System.out.println(result);
+	        return result;
+		}    
+		
+	}
+	
+	
+	// qna_number 최대값 받아오는 메서드 -------------------- 최승우----------------------------
+	public int getMaxNum() {
+		int result = -1;
+		int cal_num = 0;
+		
+		String selectSql = "select * from calendar order by QNA_ID desc limit 1";
+		
+		try {
+			con = getConnection();
+			psmt = con.prepareStatement(selectSql);
+			
+			rs = psmt.executeQuery();
+			while(rs.next()){
+				cal_num = rs.getInt("QNA_ID");
+				
+			}
+			
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} finally {
+	        try {
+	        	if(rs != null) rs.close();
+		        if(psmt != null) psmt.close();
+		        if(con != null) con.close();
+	        } catch (SQLException e) {
+	        	// TODO Auto-generated catch block
+		        e.printStackTrace();
+	        }
+	        return cal_num + 1;
+		}    
+		
+	}
 }
