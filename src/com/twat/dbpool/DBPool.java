972be12eaf0ecabef6eb3ad6f8a12106e95b4551
빠빠이용수 <csw1266@gmail.com>
2017-07-07 03:57:47 +0900

    no message

--- src/com/twat/dbpool/DBPool.java
+++ src/com/twat/dbpool/DBPool.java
@@ -24,12 +24,9 @@ public class DBPool {
 	
 	public Connection getConnection() throws Exception {
 		Context initCtx = new InitialContext();
-<<<<<<< HEAD
+
 		DataSource ds = (DataSource) initCtx.lookup("java:comp/env/jdbc/twhat");
-=======
-		DataSource ds = (DataSource) initCtx.lookup("java:comp/env/jdbc/aclass0201");
-//		DataSource ds = (DataSource) initCtx.lookup("java:comp/env/jdbc/twhat");
->>>>>>> fcce9538ca8559abfbda06ee8b48a031f3d64cd0
+
 
 		return (Connection) ds.getConnection();
 	}
