714b523bd78ccf8d0216cc7a9e87a4ad761ca81d
빠빠이용수 <csw1266@gmail.com>
2017-07-07 04:02:32 +0900

    maxlength

--- src/com/twat/dbpool/DBPool.java
+++ src/com/twat/dbpool/DBPool.java
@@ -25,13 +25,9 @@ public class DBPool {
 	public Connection getConnection() throws Exception {
 		Context initCtx = new InitialContext();
 
-<<<<<<< HEAD
+
 		DataSource ds = (DataSource) initCtx.lookup("java:comp/env/jdbc/twhat");
-=======
-		
-		DataSource ds = (DataSource) initCtx.lookup("java:comp/env/jdbc/aclass0201");
-//		DataSource ds = (DataSource) initCtx.lookup("java:comp/env/jdbc/twhat");
->>>>>>> ce7f9e5af48c7b37f4b4480ffb21371317ec59a4
+
 
 
 		return (Connection) ds.getConnection();
